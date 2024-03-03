80a72d22cc7e8399441f320866deb638b64d3588
Sergio Callegari <sergio.callegari@gmail.com>
2024-03-03 18:04:15 +0100

    Improve packaging script

--- package.sh
+++ package.sh
@@ -7,8 +7,34 @@ TESTFILES=("test_draftwatermark-1" "test_draftwatermark-2" \
            "test_draftwatermark-5")
 TESTENGINES=("pdflatex") 
 
-version=$(grep fileversion\{ $PKGNAME.dtx | sed 's/.*{\(.*\)}.*/\1/')
+pkg_version=$(pcregrep -o1 '\\fileversion\{(.*)}' $PKGNAME.dtx)
+pkg_date=$(pcregrep -o1 '\\filedate\{(.*)}' $PKGNAME.dtx)
 PKGVERS=$(echo $version | sed 's/\./-/')
+
+check() {    
+    echo "Found package version $pkg_version @ $pkg_date"
+    provided_info=$(pcregrep -Mo1 \
+    			     '\\ProvidesPackage{'$PKGNAME'}[^\[]*\[(.*)\]' \
+    			     $PKGNAME.dtx)
+    provided_date=$(echo $provided_info | cut -d' ' -f1)
+    provided_version=$(echo $provided_info | cut -d' ' -f2)
+    if [ "$provided_version" != "$pkg_version" ] ; then
+	echo Mismatched package version $provided_version in \\ProvidesPackage \
+	     >&2
+	exit 1
+    fi
+    if [ "$provided_date" != "$pkg_date" ] ; then
+	echo Mismatched package date $provided_date in \\ProvidesPackage \
+	     >&2
+	exit 1
+    fi
+    mapfile -t changes_info \
+	    < <(pcregrep -Mo \
+    			 '\\changes{R'$pkg_version'}{[^{}]*}' $PKGNAME.dtx)
+    if [ ${#changes_info[@]} -eq 0 ]; then
+	echo Cannot find change info for package. >&2
+    fi
+}
 
 build () {
     mkdir -p buildpkg/workdir
@@ -59,7 +85,8 @@ clean() {
     rm -fr ./workdir ./testdir ./"$PKGNAME"_"$PKGVERS" ./"$PKGNAME".tds.zip
     cd -
 }
-    
+
+check
 build
 do_test
 package
